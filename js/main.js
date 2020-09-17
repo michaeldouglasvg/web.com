817a6ccc7e0a9314c399c96b0b74867367db99df
unknown <AERC@DESKTOP-GPS3NF2.aercafrica.local>
2020-09-17 23:00:27 +0300

    admin web

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $("#text-search").on("keyup", function() {
+
+      var value = $(this).val().toLowerCase();
+      console.log(value);
+      $(".item, h3, p").filter(function() {
+
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+
+      });
+
+    });
+
+});
