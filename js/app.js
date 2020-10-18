24b9fc2590ea3fc3d2d46baa98fe20025f5f0321
unknown <AERC@DESKTOP-GPS3NF2.aercafrica.local>
2020-10-18 12:26:15 +0300

    admin web

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#set").click(function(){
+        swal("Welcome", "Sorry this button is under maintainance that activates default, view in 2D and view in 3D. When done you will enjoy the service. You are welcome", "success");
+    })
+})
---
+++ js/index2.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".center-fit").click(function(){
+        swal("Oops", "The content is yet to be uploaded, but meanwhile you can visit my gallery. Place your mouse to pointer and click the button to view my gallery", "success");
+    })
+})
