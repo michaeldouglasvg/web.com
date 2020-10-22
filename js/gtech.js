4a73e4f964d44df4da185d4b8d9adaa183dc2e97
unknown <AERC@DESKTOP-GPS3NF2.aercafrica.local>
2020-10-22 10:16:21 +0300

    admin web

---
+++ js/gtech.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".promo").click(function(){
+        swal("Hello there...Read the info", "Thanks for clicking the link above for Gtech Amour website, my pleasure to inform you that this website is under maintainance, The program and database will be uploaded on 20th December 2020. You are welcome...great time Goodbye.", "info");
+    })
+})
--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-$(".hamburger .fas").click(function(){
-    $(".wrapper").addClass("active")
-})
 
-$(".wrapper .sidebar .close").click(function(){
-    $(".wrapper").removeClass("active")
+$(document).ready(function{
+    $(".hamburger .fas").click(function(){
+        $(".wrapper").addClass("active")
+    })
+    
+    $(".wrapper .sidebar .close").click(function(){
+        $(".wrapper").removeClass("active")
+    })
+    
+    function myFunction() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+    }
 })
-
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
